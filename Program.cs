3210b658ac6008b6b8d0a858ec273922368c8b6c
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-11-04 22:34:50 -0300

    to terminando ainda

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("\nínicio:");
+int numeroInicial = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nFim:");
+int numeroFinal = Convert.ToInt32(Console.ReadLine());
+
+if (numeroFinal < numeroInicial)
+{
+    Console.WriteLine("mano... faz isso direito!");
+}
+else
+{
+    while (numeroInicial <= numeroFinal)
+    {
+        numeroInicial += numeroInicial += numeroInicial += numeroInicial += numeroInicial += numeroInicial+= numeroInicial += numeroInicial += numeroInicial += numeroInicial;
+        Console.Write($"\n{numeroInicial} ");
+        
+    }
+}
+Console.WriteLine();
